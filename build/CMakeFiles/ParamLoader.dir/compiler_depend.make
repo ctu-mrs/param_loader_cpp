a8a196c655f7bdb5ac3580b3e806eba08f01c2c0
Afzal Ahmad <afzalhmd@gmail.com>
2025-09-25 19:42:08 +0200

    fixed: gitignored build

---
+++ build/CMakeFiles/ParamLoader.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for ParamLoader.
+# This may be replaced when dependencies are built.
---
+++ build/param_loader_cppConfig.cmake
@@ -0,0 +1,32 @@
+
+####### Expanded from @PACKAGE_INIT@ by configure_package_config_file() #######
+####### Any changes to this file will be overwritten by the next CMake run ####
+####### The input file was param_loader_cppConfig.cmake.in                            ########
+
+get_filename_component(PACKAGE_PREFIX_DIR "${CMAKE_CURRENT_LIST_DIR}/../../../" ABSOLUTE)
+
+macro(set_and_check _var _file)
+  set(${_var} "${_file}")
+  if(NOT EXISTS "${_file}")
+    message(FATAL_ERROR "File or directory ${_file} referenced by variable ${_var} does not exist !")
+  endif()
+endmacro()
+
+macro(check_required_components _NAME)
+  foreach(comp ${${_NAME}_FIND_COMPONENTS})
+    if(NOT ${_NAME}_${comp}_FOUND)
+      if(${_NAME}_FIND_REQUIRED_${comp})
+        set(${_NAME}_FOUND FALSE)
+      endif()
+    endif()
+  endforeach()
+endmacro()
+
+####################################################################################
+
+include("${CMAKE_CURRENT_LIST_DIR}/param_loader_cppTargets.cmake")
+check_required_components("param_loader_cpp")
+
+# Define variables for include directories and library paths
+set_and_check(param_loader_cpp_INCLUDE_DIRS "${PACKAGE_PREFIX_DIR}/include")
+set_and_check(param_loader_cpp_LIB_DIRS "${PACKAGE_PREFIX_DIR}/lib")
